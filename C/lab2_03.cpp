9909e9b4af076702fda0cef426ad1cef9a87f07b
nguyencongluat <nguyencongluat092001@gmail.com>
2020-09-14 20:23:31 +0700

    lab 2
    tesst

---
+++ C/lab2_03.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+ int main(){
+	float a,b;
+	printf("Nhap gia tri nguyen 1=  ");
+	scanf("%f", &a);
+	printf("\nNhap gia tri so nguyen 2=  ");
+	scanf("%f", &b);
+	printf("Tong: %f", a+b);
+	printf("\nHieu: %f", a-b);
+	printf("\nTich: %f", a*b);
+	printf("\nThuong: %f", a/b);
+
+}
